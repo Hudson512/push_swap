3b7131a78456aeffe0211a4198fce13bada3b8a9
Hudson Mateque <72740537+Hudson512@users.noreply.github.com>
2024-07-24 13:08:06 +0100

    new 24 jul

--- srcs/free_memory_funct.c
+++ srcs/free_memory_funct.c
@@ -6,88 +6,88 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 08:50:31 by codespace         #+#    #+#             */
-/*   Updated: 2024/07/24 09:23:44 by codespace        ###   ########.fr       */
+/*   Updated: 2024/07/24 10:44:38 by hmateque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
-void    libera_matrix(char **str)
+void	libera_matrix(char **str)
 {
-    int i;
+	int	i;
 
-    if (str == NULL || str[0] == NULL)
-        return;
-    i = 0;
-    while (str[i])
-    {
-        free(str[i]);
-        i++;
-    }
+	if (str == NULL || str[0] == NULL)
+		return ;
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
 }
 
-void    libera_stack(t_list **a)
+void	libera_stack(t_list **a)
 {
-    t_list  *temp;
+	t_list	*temp;
 
-    if (a == NULL || *a == NULL)
-        return;
-    while (*a)
-    {
-        temp = *a;
-        *a = (*a)->next;
-        free(temp);
-    }
+	if (a == NULL || *a == NULL)
+		return ;
+	while (*a)
+	{
+		temp = *a;
+		*a = (*a)->next;
+		free(temp);
+	}
 }
 
-int     find_min(t_list *head)
+int	find_min(t_list *head)
 {
-    t_list *current;
-    int min_value;
+	t_list	*current;
+	int		min_value;
 
-    if (head == NULL)
-        return (0);
-    min_value = head->data;
-    current = head->next;
-    while (current != NULL)
-    {
-        if (current->data < min_value)
-            min_value = current->data;
-        current = current->next;
-    }
-    return min_value;
+	if (head == NULL)
+		return (0);
+	min_value = head->data;
+	current = head->next;
+	while (current != NULL)
+	{
+		if (current->data < min_value)
+			min_value = current->data;
+		current = current->next;
+	}
+	return (min_value);
 }
 
-int     find_max(t_list *head)
+int	find_max(t_list *head)
 {
-    t_list *current;
-    int max_value;
+	t_list	*current;
+	int		max_value;
 
-    if (head == NULL)
-        return (0);
-    max_value = head->data;
-    current = head->next;
-    while (current != NULL)
-    {
-        if (current->data > max_value)
-            max_value = current->data;
-        current = current->next;
-    }
-    return max_value;
+	if (head == NULL)
+		return (0);
+	max_value = head->data;
+	current = head->next;
+	while (current != NULL)
+	{
+		if (current->data > max_value)
+			max_value = current->data;
+		current = current->next;
+	}
+	return (max_value);
 }
 
-int     find_node_index(t_list *head, int value)
+int	find_node_index(t_list *head, int value)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (head)
-    {
-        if (head->data == value)
-            return (i);
-        else
-            head = head->next;
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (head)
+	{
+		if (head->data == value)
+			return (i);
+		else
+			head = head->next;
+		i++;
+	}
+	return (0);
 }
--- srcs/ft_aux_main2.c
+++ srcs/ft_aux_main2.c
@@ -6,35 +6,36 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 09:05:52 by codespace         #+#    #+#             */
-/*   Updated: 2024/07/24 09:09:01 by codespace        ###   ########.fr       */
+/*   Updated: 2024/07/24 11:19:28 by hmateque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
-void    move_list(t_list **a)
+void	move_list(t_list **a)
 {
-    t_list  *p_a;
-    
-    p_a = *a;
-    if (checker_order_list(p_a))
-    {
-			if (p_a->data > p_a->next->data && p_a->next->data > p_a->next->next->data)
-			{
-				ft_ra(a);
-				ft_sa(a);
-			}
-			else if ((p_a->data < p_a->next->data && p_a->data < p_a->next->next->data) 
+	t_list	*p_a;
+
+	p_a = *a;
+	if (checker_order_list(p_a))
+	{
+		if (p_a->data > p_a->next->data 
+				&& p_a->next->data > p_a->next->next->data)
+		{
+			ft_ra(a);
+			ft_sa(a);
+		}
+		else if ((p_a->data < p_a->next->data && p_a->data < p_a->next->next->data) 
 				&& (p_a->next->data > p_a->next->next->data))
-			{
-				ft_rra(a);
-				ft_sa(a);
-			}
-			else if (p_a->data > p_a->next->data && p_a->data < p_a->next->next->data)
-				ft_sa(a);
-			else if (p_a->data < p_a->next->data && p_a->data > p_a->next->next->data)
-				ft_rra(a);
-			else if (p_a->data > p_a->next->data && p_a->next->data < p_a->next->next->data)
-				ft_ra(a);
-    }
+		{
+			ft_rra(a);
+			ft_sa(a);
+		}
+		else if (p_a->data > p_a->next->data && p_a->data < p_a->next->next->data)
+			ft_sa(a);
+		else if (p_a->data < p_a->next->data && p_a->data > p_a->next->next->data)
+			ft_rra(a);
+		else if (p_a->data > p_a->next->data && p_a->next->data < p_a->next->next->data)
+			ft_ra(a);
+	}
 }
